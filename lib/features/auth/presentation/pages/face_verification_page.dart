d24276bf2985a1771104bebb57f869ddfa57ebf3
Yayan Heryanto <yayan.heryanto@megafinance.com>
2026-06-17 15:55:23 +0700

    change constant directory

--- lib/features/auth/presentation/pages/face_verification_page.dart
+++ lib/features/auth/presentation/pages/face_verification_page.dart
@@ -5,7 +5,7 @@ import 'package:emas/core/constants/tokens/radius_tokens.dart';
 import 'package:emas/core/constants/tokens/app_spacings.dart';
 import 'package:emas/core/utils/account_type.dart';
 import 'package:emas/core/utils/app_form_utils.dart';
-import 'package:emas/core/utils/images/app_images.dart';
+import 'package:emas/core/constants/app_images.dart';
 import 'package:emas/shared/layouts/app_scaffold_wrapper.dart';
 import 'package:emas/shared/theme/app_colors.dart';
 import 'package:emas/shared/widgets/appbar/app_page_bar.dart';
--- lib/features/auth/presentation/pages/ktp_verification_page.dart
+++ lib/features/auth/presentation/pages/ktp_verification_page.dart
@@ -7,7 +7,7 @@ import 'package:emas/core/di/injection.dart';
 import 'package:emas/core/services/camera_service.dart';
 import 'package:emas/core/utils/account_type.dart';
 import 'package:emas/core/utils/app_form_utils.dart';
-import 'package:emas/core/utils/images/app_images.dart';
+import 'package:emas/core/constants/app_images.dart';
 import 'package:emas/shared/layouts/app_scaffold_wrapper.dart';
 import 'package:emas/shared/theme/app_colors.dart';
 import 'package:emas/shared/widgets/appbar/app_page_bar.dart';
